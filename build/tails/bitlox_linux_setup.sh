a25eb4bc0bc5103d046907c016f43e06a02cb6ef
hideoussquid <thesquid@cyberspace.org>
2016-12-03 15:05:45 +0800

    removed build folder

--- build/tails/bitlox_linux_setup.sh
+++
@@ -1,6 +0,0 @@
-echo 'KERNEL=="hidraw*", SUBSYSTEM=="hidraw", MODE="0666", GROUP="plugdev"' > /etc/udev/rules.d/99-hidraw-permissions.rules
-echo 'ACTION=="add|change", SUBSYSTEM=="usb", ATTRS{idVendor}=="03eb", ATTRS{idProduct}=="204f", MODE="0666", GROUP="plugdev"' > /etc/udev/rules.d/90-bitlox.rules
-chmod 644 /etc/udev/rules.d/90-bitlox.rules
-/etc/init.d/udev restart
-
-
